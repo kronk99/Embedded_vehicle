3ca85cb9849b9dd04a82ba578b563387343d4983
kronk99 <avenapop1429@gmail.com>
2025-09-21 22:35:52 -0600

    añadido del main

--- src/streamAPI.py
+++ src/streamAPI.py
@@ -27,6 +27,9 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#para pruebas, eliminar despues
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 
     
