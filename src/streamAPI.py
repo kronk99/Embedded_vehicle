b2c82422d02c521d1bf8fd1d0dcacf79a50ebaa1
Luis González <92146862+kronk99@users.noreply.github.com>
2025-09-24 07:53:50 -0600

    Update streamAPI.py

--- src/streamAPI.py
+++ src/streamAPI.py
@@ -1,35 +1,35 @@
-from flask import Flask , Response
-from picamera2 import picamera2
-import cv2
+from flask import Flask, Response, jsonify
+from picamera import PiCamera
+import io
+import time
+#considerar usos de libjpeg-turbo en la imagen REVISARLOCAL.CONFIG
 app = Flask(__name__)
 
-camara = Picamera2()
-camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888',"size": (640,480)}))
-camera.start()
-
-#generador de frames, esto va a tomar capturas por cada ciclo while (rapido) cv2 va a agarrar el array
-#de fotos .jpg y lo va a convertir en data stremeable (en un buffer)
-#frame contiene los bytes de la imagen 
-#yield manda la data.
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 24
+time.sleep(0.1)
 
 def generate_frames():
-    while True:
-        frame = camera.capture_array()
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+    stream = io.BytesIO()
+    for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+        stream.seek(0)
+        frame_bytes = stream.read()
+        stream.seek(0)
+        stream.truncate()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
 @app.route("/")
 def index():
     return jsonify({"status": "API streaming Flask activa 🚀"})
-    
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-#para pruebas, eliminar despues
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-    
+    try:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
+    finally:
+        camera.close()
